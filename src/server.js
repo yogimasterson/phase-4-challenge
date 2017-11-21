cd0991ce3780e43d2b5ad6ad3a4a6a6f4ebc98c7
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-11-20 17:26:44 -0800

    rebase off of project scaffold

--- src/server.js
+++ src/server.js
@@ -1,17 +1,46 @@
+const path = require('path')
 const express = require('express')
-const app = express()
-
-const routes = require('./routes/')
+const bodyParser = require('body-parser')
+const db = require('./db')
 
 const port = process.env.PORT || 3000
 
+const app = express()
+
+require('ejs')
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', routes)
+app.get('/', (req, res) => {
+  db.getAlbums((error, albums) => {
+    if (error) {
+      res.status(500).render('error', {error})
+    } else {
+      res.render('index', {albums})
+    }
+  })
+})
+
+app.get('/albums/:albumID', (req, res) => {
+  const albumID = req.params.albumID
+
+  db.getAlbumsByID(albumID, (error, albums) => {
+    if (error) {
+      res.status(500).render('error', {error})
+    } else {
+      const album = albums[0]
+      res.render('album', {album})
+    }
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).render('not_found')
+})
 
 app.listen(port, () => {
-    console.log(`Listening on port: http://localhost:${port}`)
+  console.log(`Listening on http://localhost:${port}...`)
 })
