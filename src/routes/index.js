a2a12def901de7cd30bf36eb8aa2339237382ece
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-11-20 18:01:04 -0800

    extracts route out of server.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router()
+const db = require('../db')
 
 router.get('/', (req, res) => {
-    res.render('index')
+  db.getAlbums((error, albums) => {
+    if (error) {
+      res.status(500).render('error', {
+        error,
+      })
+    } else {
+      res.render('index', {
+        albums,
+      })
+    }
+  })
+})
+
+router.get('/albums/:albumID', (req, res) => {
+  const albumID = req.params.albumID
+
+  db.getAlbumsByID(albumID, (error, albums) => {
+    if (error) {
+      res.status(500).render('error', {
+        error,
+      })
+    } else {
+      const album = albums[0]
+      res.render('album', {
+        album,
+      })
+    }
+  })
 })
 
 module.exports = router
