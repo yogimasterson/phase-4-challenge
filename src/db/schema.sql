e31b8bcd530bae0c80db029176a68f459589f5c0
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-11-20 18:46:11 -0800

    adds drop table sql command

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,8 +1,12 @@
+DROP TABLE IF EXISTS albums;
+
 CREATE TABLE albums (
   id SERIAL,
   title VARCHAR(255) NOT NULL,
   artist VARCHAR(255) NOT NULL
 );
+
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id SERIAL,
@@ -11,6 +15,8 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL,
   join_date TIMESTAMP(0) DEFAULT now()
 );
+
+DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE reviews (
   id SERIAL,
